3f4db8a395c486ca6577a880fff99f00d16d1752
Oscar Castillo <oscar_cm@outlook.com>
2023-05-16 12:26:09 -0600

    install emotion-react and reset styles

---
+++ src/styles/colors.js
@@ -0,0 +1,11 @@
+export const colors = {
+    gray: {
+      dark: '#B8B8BB',
+      medium: '#F6F6F9',
+      light: '#F6F6F9',
+    },
+    black: '#333333',
+    white: '#FFFFFF',
+    background: '#BCBABA',
+  };
+  
