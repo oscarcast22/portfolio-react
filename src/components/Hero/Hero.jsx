3031e1174d26527580c087c6f287f4fa393b435d
Oscar Castillo <oscar_cm@outlook.com>
2023-10-23 12:22:19 -0600

    correct styles of hero

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -81,15 +81,24 @@ const Curve = styled.div`
   }
 `
 
+const Text = styled.div`
+  width: 100%;
+  max-width: 1200px;
+  margin-bottom: 36px;
+  @media (max-width: 623px) {
+    margin-bottom: 100px;
+  }
+`
+
 function Hero() {
 
   return (
     <>
       <Container>
-        <div style={{ width: '100%', maxWidth: '1200px', margin: 'auto', marginBottom:'60px' }}>
+        <Text>
           <Dotgrid></Dotgrid>
           <Presentation/>
-        </div>
+        </Text>
         <Curve>
           <svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
             <path d="M321.39,56.44c58-10.79,114.16-30.13,172-41.86,82.39-16.72,168.19-17.73,250.45-.39C823.78,31,906.67,72,985.66,92.83c70.05,18.48,146.53,26.09,214.34,3V0H0V27.35A600.21,600.21,0,0,0,321.39,56.44Z" style={{fill: '#4d4d4d'}}></path>
